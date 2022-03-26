15a0fc15b95e275b47db162a61e7afc671c5a084
Igor Kulgachov <duckpool@yandex.ru>
2022-03-26 18:08:34 +0300

    initial commit

---
+++ src/main/java/com/liga/internship/server/PrerevolutionaryTinderServerApplication.java
@@ -0,0 +1,13 @@
+package com.liga.internship.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrerevolutionaryTinderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrerevolutionaryTinderServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liga/internship/server/PrerevolutionaryTinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liga.internship.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrerevolutionaryTinderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
